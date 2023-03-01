e58987cd794badc864dfa1fb4557bd8c700b00bc
WeigengLi <liweigeng2014@hotmail.com>
2023-03-01 18:00:33 -0500

    project2 update

--- 6582AI/Project2/code.py
+++ 6582AI/Project2/code.py
@@ -1,4 +1,22 @@
+import os
+class Node:
+    pass
 
+def load_text(filename):
+    # for read in files
+    if not os.path.exists(filename):
+        print('File '+filename+' does not exist')
+        return
+    pitchers = []
+    file= open(filename,'r').readlines()
+    Adjacent_Matrix = [] 
+    for line in file:
+        line.replace("\n","")
+        for number in line.strip().split(','):
+            if number.isdigit():
+                pitchers.append(int(number))
+    target= int(file[1])
+    return pitchers,target
 
 def main():
     pass
