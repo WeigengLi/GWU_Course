fc6d54fd09a447b03bb66dc6afb4f77eb1a8b7b8
WeigengLi <liweigeng2014@hotmail.com>
2023-03-04 23:04:36 -0500

    AI Project2: update test data and  load_text()

--- 6582AI/Project2/code.py
+++ 6582AI/Project2/code.py
@@ -7,16 +7,35 @@ def load_text(filename):
     if not os.path.exists(filename):
         print('File '+filename+' does not exist')
         return
-    pitchers = []
     file= open(filename,'r').readlines()
-    Adjacent_Matrix = [] 
+    Adjacent_list = dict()
+    color = -1
     for line in file:
+        if line == '':
+            continue
         line.replace("\n","")
-        for number in line.strip().split(','):
-            if number.isdigit():
-                pitchers.append(int(number))
-    target= int(file[1])
-    return pitchers,target
+        words = line.strip().split(',')
+        if words[0] == '#':
+            continue
+        if words[0] == 'color':
+            color = int(words[0])
+            continue
+
+        if words[0].isdigit():
+            number1, number2 = int(words[0]),int(words[1])
+            if words[0] in Adjacent_list.key():
+                Adjacent_list[number1].add(number2)
+            else:
+                Adjacent_list[number1] = set()
+                Adjacent_list[number1].add(number2)
+            if words[1] in Adjacent_list.key():
+                Adjacent_list[number2].add(number1)
+            elif words[1].isdigit():
+                Adjacent_list[number2] = set()
+                Adjacent_list[number2].add(number1)
+            
+    
+    return color,Adjacent_list
 
 def main():
     pass
